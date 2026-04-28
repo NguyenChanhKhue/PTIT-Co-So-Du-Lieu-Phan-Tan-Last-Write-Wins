6c480d32c885578e7a7685c2d3464875e59c259b
NguyenChanhKhue <chanhkhue7122005@gmail.com>
2026-04-28 10:25:52 +0700

    first commit

---
+++ backend/src/main/java/com/example/distributed_last_write_wins/DistributedLastWriteWinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributed_last_write_wins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLastWriteWinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedLastWriteWinsApplication.class, args);
+	}
+
+}
